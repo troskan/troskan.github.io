85d284ae67eff56054d02bd9c1ae965f71e48162
Trosk <Str4ndb3rg@live.se>
2023-02-11 12:00:24 +0100

    fixed quiz game to actually work when click buttons

--- js/webquiz.js
+++ js/webquiz.js
@@ -18,30 +18,69 @@ answers[2] = [
   "There is no punishment.",
 ];
 
-let maxQuestions = 2;
-let currentQuestionCount = 1;
+let maxQuestions = 3;
+let currentQuestionCount = 0;
 let inputAnswer = "";
 let currentQuestion = "";
 let currentQuestionID;
-let randomID = Math.floor(Math.random() * questions.length);
+
+//Buttons
+var inputButtons = [];
+
+var answerArr = [];
 
 function displayQuestion() {
-  currentQuestion = document.getElementById("question").innerHTML =
+  let randomID = Math.floor(Math.random() * questions.length);
+  currentQuestionID = document.getElementById("question").innerHTML =
     questions[randomID];
-  currentQuestionID = randomID;
-
+  //questions.splice(randomID, 1);
   return currentQuestion;
 }
-function clickAnswer() {
-  var answerArr = [];
-  answerArr[0] = document.getElementById("answer-0").innerHTML =
-    answers[randomID][0];
-  answerArr[1] = document.getElementById("answer-1").innerHTML =
-    answers[randomID][1];
-  answerArr[2] = document.getElementById("answer-2").innerHTML =
-    answers[randomID][2];
-  return answerArr;
+function displayAnswer() {
+  currentQuestionCount++;
+  document.getElementById("current-question-count").innerHTML =
+    currentQuestionCount;
+
+  let randomID = Math.floor(Math.random() * answers.length);
+  currentQuestionID = randomID;
+
+  document.getElementById("question").innerHTML = questions[randomID];
+
+  document.getElementById("answer-0").innerHTML = answers[randomID][0];
+  document.getElementById("answer-0").value = answers[randomID][0];
+
+  document.getElementById("answer-1").innerHTML = answers[randomID][1];
+  document.getElementById("answer-1").value = answers[randomID][1];
+
+  document.getElementById("answer-2").innerHTML = answers[randomID][2];
+  document.getElementById("answer-2").value = answers[randomID][2];
+  //answers.splice(randomID, 1);
+}
+function whenAnswerClick(button) {
+  input = button.value;
+  if (input === answers[0][0]) {
+    displayQuestion();
+    displayAnswer();
+  } else if (input === answers[1][0]) {
+    displayQuestion();
+    displayAnswer();
+  } else if (input === answers[2][2]) {
+    displayAnswer();
+    displayQuestion();
+  } else {
+    console.log(input);
+    console.log("else reached! WARNING");
+  }
 }
 
-displayQuestion();
-clickAnswer();
+document.getElementById("answer-0").addEventListener("click", function () {
+  whenAnswerClick(this);
+});
+document.getElementById("answer-1").addEventListener("click", function () {
+  whenAnswerClick(this);
+});
+document.getElementById("answer-2").addEventListener("click", function () {
+  whenAnswerClick(this);
+});
+
+displayAnswer();
