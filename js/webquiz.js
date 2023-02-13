f44ca4e4b8c66dba4b3e20f33578e976b26a708a
troskan <str4ndb3rg@live.se>
2023-02-13 16:35:02 +0100

    added more answers

--- js/webquiz.js
+++ js/webquiz.js
@@ -19,32 +19,110 @@
 // ];
 var questions = [
   {
-    question: "What are the colors of the Greek flag?",
-    answers: ["White and blue", "Green, red and blue", "Red and green"],
-    correctAnswer: 0, // index of the correct answer
+    question: "What is the capital city of Australia?",
+    answers: ["Sydney", "Canberra", "Melbourne"],
+    correctAnswer: 1,
   },
   {
-    question: "Does beavers exist in Sweden?",
-    answers: [
-      "Yes they exist in Sweden",
-      "No",
-      "Yes they exist, but only when they swim from Canada",
-    ],
-    correctAnswer: 0, // index of the correct answer
+    question: "What is the largest country in South America by land area?",
+    answers: ["Argentina", "Brazil", "Colombia"],
+    correctAnswer: 1,
+  },
+  {
+    question: "What is the main ingredient in hummus?",
+    answers: ["Chickpeas", "Lentils", "Black beans"],
+    correctAnswer: 0,
+  },
+  {
+    question: "What is the world's largest ocean?",
+    answers: ["Atlantic Ocean", "Arctic Ocean", "Pacific Ocean"],
+    correctAnswer: 2,
   },
   {
     question:
-      "In Sweden, what is the punishment for going to church with a hat on?",
-    answers: [
-      "$100 citation",
-      "Jail time, 1 week to 9 weeks",
-      "There is no punishment",
-    ],
-    correctAnswer: 2, // index of the correct answer
+      "What is the name of the famous detective created by Sir Arthur Conan Doyle?",
+    answers: ["Sherlock Holmes", "Hercule Poirot", "Nancy Drew"],
+    correctAnswer: 0,
+  },
+  {
+    question: "What is the smallest planet in our solar system?",
+    answers: ["Venus", "Mercury", "Mars"],
+    correctAnswer: 1,
+  },
+  {
+    question: "What is the chemical symbol for gold?",
+    answers: ["Go", "Gr", "Au"],
+    correctAnswer: 2,
+  },
+  {
+    question: "What is the highest mountain in Africa?",
+    answers: ["Mount Kilimanjaro", "Mount Kenya", "Mount Elgon"],
+    correctAnswer: 0,
+  },
+  {
+    question: "What is the largest organ in the human body?",
+    answers: ["Liver", "Skin", "Heart"],
+    correctAnswer: 1,
+  },
+  {
+    question: "What is the most widely spoken language in the world?",
+    answers: ["Mandarin Chinese", "Spanish", "English"],
+    correctAnswer: 0,
+  },
+  {
+    question: "What is the largest planet in our solar system?",
+    answers: ["Jupiter", "Saturn", "Uranus"],
+    correctAnswer: 0,
+  },
+  {
+    question: "What is the name of the first man to walk on the moon?",
+    answers: ["Neil Armstrong", "Buzz Aldrin", "Michael Collins"],
+    correctAnswer: 0,
+  },
+  {
+    question: "What is the only continent that is also a country?",
+    answers: ["Australia", "South America", "Europe"],
+    correctAnswer: 0,
+  },
+  {
+    question: "What is the currency of Japan?",
+    answers: ["Yuan", "Euro", "Yen"],
+    correctAnswer: 2,
+  },
+  {
+    question: "What is the tallest mammal in the world?",
+    answers: ["Giraffe", "Elephant", "Hippopotamus"],
+    correctAnswer: 0,
+  },
+  {
+    question: "What is the process by which plants make their own food called?",
+    answers: ["Respiration", "Photosynthesis", "Digestion"],
+    correctAnswer: 1,
+  },
+  {
+    question: "What is the name of the world's largest desert?",
+    answers: ["Sahara", "Gobi", "Arabian"],
+    correctAnswer: 0,
+  },
+  {
+    question: "What is the chemical symbol for oxygen?",
+    answers: ["O", "C", "H"],
+    correctAnswer: 0,
+  },
+  {
+    question:
+      "What is the name of the famous ship that sank on its maiden voyage in 1912?",
+    answers: ["Titanic", "Lusitania", "Andrea Doria"],
+    correctAnswer: 0,
+  },
+  {
+    question: "What is the name of the world's largest coral reef system?",
+    answers: ["Great Barrier Reef", "Coral Triangle", "Belize Barrier Reef"],
+    correctAnswer: 0,
   },
 ];
 
-let maxQuestions = 3;
+let maxQuestions = questions.length;
 let currentQuestionCount = 0;
 let inputAnswer = "";
 let currentQuestion = "";
@@ -64,15 +142,13 @@ if (score < 0) {
 
 function displayQuestion() {
   questionCounter++;
-  if (questionCounter > questions.length - 1) {
+  if (questionCounter > questions.length + 1) {
     location.reload();
   }
 
   let randomID = Math.floor(Math.random() * questions.length);
   currentQuestionID = document.getElementById("question").innerHTML =
     questions[randomID];
-  //'questions.splice(randomID, 1);
-  return currentQuestion;
 }
 function displayAnswer() {
   currentQuestionCount++;
@@ -99,6 +175,7 @@ function displayAnswer() {
     questions[currentQuestionID].answers[2];
   document.getElementById("answer-2").value =
     questions[currentQuestionID].correctAnswer === 2 ? "correct" : "incorrect";
+  questions.splice(randomID, 1);
 }
 function whenAnswerClick(button) {
   input = button.value;
